baaca305e094b03580eddec540b5a21f015129d2
D-Dark-Knight <33816067+D-Dark-Knight@users.noreply.github.com>
2017-11-22 23:14:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
-#!/bin/sh
-
 sudo apt-get install -y wget
 sudo apt-get install -y python-pip
-
 pip install azure
-
-
-wget https://github.com/j-l-m/testrepo/raw/master/rec_msgs.py
-
-nohup python rec_msgs.py &
-nohup python rec_msgs.py &
-
+wget https://raw.githubusercontent.com/D-Dark-Knight/Cloud-Assignment-3/master/recieverA3T.py
+nohup python recieverA3T.py &
+nohup python recieverA3T.py &
