bccedc21edb119dff1eb72903400b2ea93d64a9a
D-Dark-Knight <33816067+D-Dark-Knight@users.noreply.github.com>
2017-11-25 22:58:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt-get install -y wget
 sudo apt-get install -y python-pip
 pip install azure
+pip install flask
 wget https://raw.githubusercontent.com/D-Dark-Knight/Cloud-Assignment-3/master/recieverA3T.py
 python recieverA3T.py
