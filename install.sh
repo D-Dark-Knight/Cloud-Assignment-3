20d5b6b7da7b8a6e0ce112ab6dc94ac7a2a52b30
D-Dark-Knight <33816067+D-Dark-Knight@users.noreply.github.com>
2017-11-25 22:47:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@ sudo apt-get install -y wget
 sudo apt-get install -y python-pip
 pip install azure
 wget https://raw.githubusercontent.com/D-Dark-Knight/Cloud-Assignment-3/master/recieverA3T.py
-nohup python recieverA3T.py &
-nohup python recieverA3T.py &
+python recieverA3T.py
