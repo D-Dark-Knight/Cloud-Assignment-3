3e930df087d28771ec93c7572fe388a6c82b8420
D-Dark-Knight <33816067+D-Dark-Knight@users.noreply.github.com>
2017-12-05 00:03:49 -0400

    Update recieverA3T.py

--- recieverA3T.py
+++ recieverA3T.py
@@ -2,54 +2,47 @@ from azure.servicebus import ServiceBusService, Message, Queue
 from multiprocessing import Pool
 import thread
 from azure.storage.table import TableService, Entity
-
-from flask import Flask
-app = Flask(__name__)
+import json
 
 bus_service = ServiceBusService(
-    service_namespace='batmanandrobin491fb9',
-    shared_access_key_name='ross',
-    shared_access_key_value='i7vczr8lLmA3XeyWEMfekhob0J3eWk09JUMv/qX2afs=')
+    service_namespace='rossamrit',
+    shared_access_key_name='RootManageSharedAccessKey',
+    shared_access_key_value='WQ8H90SmQA7OZEux1jpGP7bh4gZBJuVcJzooReElV74=')
 
-table_service = TableService(account_name='batmanandrobin491fb9', account_key='MPsdcQOiLZewcUIYHYtPkjRDtKrzHRq7Z/AVkIt9Zo6IvQP2/Zff4a19GXOwI4MIxOLvQ00lCIwo/lkqml4jbw==')
+table_service = TableService(account_name='amrittable', account_key='Cgqr5ZSRv/K/9ftaeQMMHzQcEL06VlyulbDtqnXD0eb6QujMkijdYzk0m37kJaswlyyVgGd9zyPw8P0ArdZy2Q==')
 
-@app.route('/')
-def hello_world(i,j ):
-	while (i < j): 
-		msg = bus_service.receive_queue_message('assignment3', peek_lock=True)
-		print(msg.body)
-		task = {'PartitionKey': "1", 'RowKey': str(i), 'description' : msg.body, 'priority' : 200}
-		table_service.insert_entity('assignment3', task)
-		msg.delete()
-		i = i +1
-	return "finished"	
+msgl = ""
+msgl2 = ""
+
+def hello_world(name):
+	while 1:
+		try:
+			msg = bus_service.receive_queue_message('ross', peek_lock=True)
+			msg1 = msg.body
+			msgl2 = json.loads(msg1)
+			print(msgl2["UserId"])
+			print(msgl2["SellerID"])
+			print(msgl2["Product Name"])
+			print(msgl2["Sale Price"])
+			print(msgl2["Transaction Date"])
+			task = {"PartitionKey"    : msgl2["TransactionID"], 
+					"RowKey"          : "Anything", 
+					"User_ID"         : msgl2["UserId"],
+					"SellerID"        : msgl2["SellerID"] ,
+					"Product Name"    : msgl2["Product Name"],
+					"Sale Price"      : msgl2["Sale Price"] }
+					
+					
+			#table_service.insert_entity('amritTable', task)
+			msg.delete()
+		except Exception as e:
+			s = str(e)
+			print(s)
+
 		
 if __name__ == '__main__':
-	try:
-	   thread.start_new_thread( hello_world, (0,30000) )
-	   thread.start_new_thread( hello_world, (30001,60000) )
-	   thread.start_new_thread( hello_world, (60001,90000) )
-	   thread.start_new_thread( hello_world, (90001,120000) )
-	   thread.start_new_thread( hello_world, (120001,150000) )
-	   thread.start_new_thread( hello_world, (150001,180000) )
-	   thread.start_new_thread( hello_world, (180001,210000) )
-	   thread.start_new_thread( hello_world, (210001,240000) )
-	   thread.start_new_thread( hello_world, (240001,270000) )
-	   thread.start_new_thread( hello_world, (270001,300000) )
-	   thread.start_new_thread( hello_world, (300001,330000) )
-	   thread.start_new_thread( hello_world, (330001,360000) )
-	   thread.start_new_thread( hello_world, (360001,390000) )
-	   thread.start_new_thread( hello_world, (390001,420000) )
-	   thread.start_new_thread( hello_world, (420001,450000) )
-	   thread.start_new_thread( hello_world, (450001,480000) )
-	   thread.start_new_thread( hello_world, (480001,510000) )
-	   thread.start_new_thread( hello_world, (510001,540000) )
-	   thread.start_new_thread( hello_world, (540001,570000) )
-	   thread.start_new_thread( hello_world, (570001,600000) )
-	except:
-	   print "Error: unable to start thread"
-	while 1:
-		app.run()
-		pass
-
+	p = Pool(10)
+	
+	(p.map(hello_world, ["dummy", "dummy", "dummy","dummy","dummy","dummy","dummy","dummy","dummy","dummy"]))
+	app.run()
 	
